a2689e1dad6051499f42c93d17dd1d2f85606179
Philip Zhang <jmslca123@gmail.com>
2021-04-21 22:55:22 -0700

    add prisma
    Co-authored-by: Herrick Fang <herrickfang@gmail.com>

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,43 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import expressPlayground from "graphql-playground-middleware-express";
+
+const prisma = new PrismaClient();
+
+const typeDefs = `
+  type User {
+    email: String!
+    name: String
+  }
+  type Query {
+    allUsers: [User!]!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    allUsers: () => {
+      return prisma.user.findMany();
+    },
+  },
+};
+export const schema = makeExecutableSchema({
+  resolvers,
+  typeDefs,
+});
+
 const app = express();
 const port = 5000;
 
-app.get("/", (_, res) => {
-  res.send("Working!!!");
-});
+app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+  })
+);
 
 app.listen(port, () => console.log(`Running on port ${port}`));
